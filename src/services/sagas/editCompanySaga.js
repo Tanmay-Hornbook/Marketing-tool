61aca078299a08af9b5de0d56a9d2aac803676fe
= <tanmay@hornbooktechnologies.in>
2023-12-05 18:33:09 +0530

    fixed missing yield in generators

--- src/services/sagas/editCompanySaga.js
+++ src/services/sagas/editCompanySaga.js
@@ -1,18 +1,20 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { EDIT_COMPANY_ACTION_TYPES } from "../actions/constants/editCompanyActionConstants";
 import { editCompanySuccess, editCompanyFailure } from "../actions/editCompanyActions";
+import { getCompanyRequest } from "../actions/companyActions";
 import axiosInstance from "../../utils/axiosConfig";
 
 function* editCompanyRequest(action) {
-  return axiosInstance.put("api/company/edit", action);
+  return yield axiosInstance.put("api/company/edit", action);
 }
 
 function* handleEditCompanyRequest(action) {
   try {
     let response = yield call(editCompanyRequest, action.payload);
     const data = yield response;
-    if (data.status === 201) {
+    if (data.status === 200) {
       yield put(editCompanySuccess(data.data));
+      yield put(getCompanyRequest());
     } else {
       yield put(editCompanyFailure("Could not update company"));
     }
