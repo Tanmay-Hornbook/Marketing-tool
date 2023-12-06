f672b3849596c917b6b57e43e99c830130870697
= <tanmay@hornbooktechnologies.in>
2023-12-06 15:29:22 +0530

    fixed jest transform error

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,10 @@
-export default {
+module.exports = {
   presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: "current",
-        },
-        modules: "auto",
-      },
-    ],
-    "@babel/preset-react",
+    '@babel/preset-env',
+    '@babel/preset-react',
   ],
-  plugins: ["@babel/plugin-transform-runtime"],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
-export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  transformIgnorePatterns: ["/node_modules/"],
-  globals: {
-    NODE_ENV: "test",
-  },
-};
+module.exports = {
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    },
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+  };
+  
