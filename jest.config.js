d4a98f6d819fa113312315afae43e072a142185c
= <tanmay@hornbooktechnologies.in>
2023-12-06 15:41:48 +0530

    fixed jest transform error

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy",
+  },
   testEnvironment: "jsdom",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$",
   moduleFileExtensions: ["js", "jsx", "json", "node"],
