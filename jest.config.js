76600f51bf8cc0a7e40d2be53f0b7996bf171506
= <tanmay@hornbooktechnologies.in>
2023-12-06 15:45:53 +0530

    fixed jest transform error

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  testEnvironment: "jsdom",
   moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy",
   },
-  testEnvironment: "jsdom",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$",
-  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/build/"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
 };
