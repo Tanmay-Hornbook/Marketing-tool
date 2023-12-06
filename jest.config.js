6de67cb0fcd1a26db8054e3fc218b64a18b29f43
= <tanmay@hornbooktechnologies.in>
2023-12-06 15:32:59 +0530

    fixed jest transform error

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    },
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-  };
-  
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+};
