76dd05ee9f12c4590c67ece6f4ec9c34184bb177
= <tanmay@hornbooktechnologies.in>
2023-12-11 13:54:07 +0530

    modified interceptor

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { API_URL } from "../config";
 import { getAccessToken } from "./Utils";
+import { storeAccessToken } from "./Utils";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
@@ -24,4 +25,25 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const response = await axiosInstance.post("api/auth/refresh-token", refreshToken);
+        storeAccessToken(response.data);
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        console.log("console_error", error);
+        return Promise.reject(error);
+      }
+    }
+  }
+);
 export default axiosInstance;
